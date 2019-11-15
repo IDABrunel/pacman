6f077780284847cde9b0ed3f4e15e26a1bdd4ae2
Ben Evans <ben@bluechimp.io>
2019-11-15 21:33:12 +0000

    feat: python api to handle batch op write + op end feedback

--- game/arduino.py
+++ game/arduino.py
@@ -1,61 +1,144 @@
 import serial
 import time
+from random import shuffle
+
+
+def generate_empty_rgb_matrix(width, height):
+    matrix = []
+    for _ in range(0, height):
+        row = []
+        for _ in range(0, width):
+            row.append([0, 0, 0])
+        matrix.append(row)
+    return matrix
+
 
 class ArduinoRGBMatrix:
 
-    def __init__(self, init_state, width, height, row_reverse):
-        self.width = width
-        self.current_state = init_state
-        # self.send_full()
+    def __init__(self):
+        self.width = 60
+        self.height = 20
+        self.current_state = generate_empty_rgb_matrix(self.width, self.height)
         self.serial = serial.Serial('/dev/ttyACM0', 9600)
-        time.sleep(3)
+        time.sleep(1)
 
+    ###
+    # Low-level ops.
+    ###
 
-    def send_pixel(self, location, rgb):
+    def send_batch_ops(self, op_arrs):
+        ops_str = ''
+
+        for op_arr in op_arrs:
+            op_str = ''
+
+            for i in op_arr:
+                op_str = op_str + str(i) + ','
+
+            ops_str = ops_str + op_str + '\n'
+
+        self.serial.write(bytes(ops_str, 'ascii'))
+
+        for _ in op_arrs:
+            self.serial.read_until(b'E')
+
+    def send_op(self, op_arr):
+        self.send_batch_ops([op_arr])
+
+    def op_clear(self):
+        return [0]
+
+    def op_set(self, iloc, r, g, b):
+        return [1, iloc, r, g, b]
+
+    def op_show(self):
+        return [2]
+
+    def location_to_iloc(self, location):
+        [x, y] = location
+
+        if (y % 2) == 1:
+            return (y * (self.width)) + self.width - x - 1
+        else:
+            return (y * self.width) + x
+
+    ###
+    # High Level Ops
+    ###
+
+    def clear(self):
+        self.send_batch_ops([
+            self.op_clear(),
+            self.op_show()
+        ])
+
+    def set_pixel(self, location, rgb):
         r, g, b = rgb
-        print("{},{},{},{}".format(location, r, g, b))
-        print(self.serial.write(bytes('{},{},{},{}\n'.format(location, r, g, b), 'ascii')))
-        time.sleep(.05)
+        [x, y] = location
 
-    def send_full(self, rgb_matrix):
-        for (y, row) in enumerate(rgb_matrix):
-            for (x, rgb) in enumerate(row):
-                if (y % 2) == 1:
-                    self.send_pixel((y * (self.width)) + self.width - x - 1, [rgb[0], rgb[1], rgb[2]])
-                    # print('{},{},{},{}'.format((y * self.width) + self.width - x, rgb[0], rgb[1], rgb[2]))
-                else:
-                    self.send_pixel((y * self.width) + x, [rgb[0], rgb[1], rgb[2]])
-                    # print('{},{},{},{}'.format((y * self.width) + x, rgb[0], rgb[1], rgb[2]))
+        self.send_batch_ops([
+            self.op_set(self.location_to_iloc(location), r, g, b),
+            self.op_show()
+        ])
 
-    def send_update(self, rgb_matrix):
-        updates = self.calculate_diff(rgb_matrix)
-        print('UPDATES')
-        for x in updates:
-            print(x)
+        self.current_state[y][x] = rgb
+
+    def update_by_pixel(self, rgb_matrix):
+        changes_from_current_state = self.calculate_diff(rgb_matrix)
+
+        for [location, rgb] in changes_from_current_state:
+            self.set_pixel(location, rgb)
+
+    def update_by_n_random_pixels(self, rgb_matrix, n=1):
+        changes_from_current_state = self.calculate_diff(rgb_matrix)
+        shuffle(changes_from_current_state)
+
+        while(len(changes_from_current_state) > 0):
+            x = n if n < len(changes_from_current_state) else len(changes_from_current_state)
+            ops = []
+
+            for _ in range(0, x):
+                [location, rgb] = changes_from_current_state.pop()
+                x, y = location
+                r, g, b = rgb
+                self.current_state[y][x] = rgb
+                ops.append(self.op_set(self.location_to_iloc(location), r, g, b))
+
+            ops.append(self.op_show())
+
+            self.send_batch_ops(ops)
+
+    def update(self, rgb_matrix):
+        """
+        Update the whole board in one batch.
+        """
+        changes_from_current_state = self.calculate_diff(rgb_matrix)
+
+        ops = []
+
+        for [location, rgb] in changes_from_current_state:
+            [r, g, b] = rgb
+            [x, y] = location
+
+            self.current_state[y][x] = rgb
+            ops.append(self.op_set(self.location_to_iloc(location), r, g, b))
+
+        ops.append(self.op_show())
+
+        self.send_batch_ops(ops)
 
     def calculate_diff(self, new_state):
+        """
+        Compared the current state with a proposed / new state and returns all
+        the pixels that differ.
+
+        Returns in the form of [ [[x, y], [r, g, b]], ... ]
+        """
         pixel_changes = []
 
         for y, row in enumerate(new_state):
             for x, pixel in enumerate(row):
-                if pixel != self.current_state[y][x]:
+                if pixel[0] != self.current_state[y][x][0] or pixel[1] != self.current_state[y][x][1] or pixel[2] != self.current_state[y][x][2]:
                     pixel_changes.append([[x, y], pixel])
 
         return pixel_changes
-
-
-# a = ArduinoRGBMatrix([], 60, 20, False)
-# import random
-# for x in range(0, 60 * 6):
-#     a.send_pixel(x, random.randint(0, 100), random.randint(0, 100), random.randint(0, 100))
-
-# a.send_pixel(0, 0,0,0)
-
-
-
-# s = serial.Serial('/dev/ttyACM0', 9600, timeout=5)
-
-
-# s.write(bytes("{},{},{},{}\n\n".format(location, r, g, b).encode('ascii')))
-# # s.flush()
-# # time.sleep(6) 
