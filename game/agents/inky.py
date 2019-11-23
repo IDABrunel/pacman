ddb1b755aa52b001d298b8a68b8892bcfb65cfb5
Mannjamin <Mann.Ashley@outlook.com>
2019-11-23 17:24:27 +0000

    fixed tunnel logic'

--- game/agents/inky.py
+++ game/agents/inky.py
@@ -13,31 +13,31 @@ class Inky:
 
         return self._location
 
-        def calculate_move_location(self, move):
-            if move == 'U':
-                return [self._location[0], self._location[1] - 1]
+    def calculate_move_location(self, move):
+        if move == 'U':
+            return [self._location[0], self._location[1] - 1]
 
-            if move == 'D':
-                return [self._location[0], self._location[1] + 1]
+        if move == 'D':
+            return [self._location[0], self._location[1] + 1]
 
-            if move == 'L':
-                if [self._location[0] - 1, self._location[1]] == [0, 5]:
-                    # print('This is a teleport')
-                    return [self._location[0] + 58, self._location[1]]
-                else:
-                    return [self._location[0] - 1, self._location[1]]
+        if move == 'L':
+            if [self._location[0] - 1, self._location[1]] == [0, 5]:
+                # print('This is a teleport')
+                return [self._location[0] + 58, self._location[1]]
+            else:
+                return [self._location[0] - 1, self._location[1]]
 
-            if move == 'R':
-                if [self._location[0] + 1, self._location[1]] == [60, 5]:
-                    # print('This is a teleport')
-                    return [self._location[0] - 59, self._location[1]]
-                else:
-                    return [self._location[0] + 1, self._location[1]]
+        if move == 'R':
+            if [self._location[0] + 1, self._location[1]] == [60, 5]:
+                # print('This is a teleport')
+                return [self._location[0] - 59, self._location[1]]
+            else:
+                return [self._location[0] + 1, self._location[1]]
 
-            if move == '':
-                return self._location
+        if move == '':
+            return self._location
 
-            raise Exception('Invalid Move Identifier')
+        raise Exception('Invalid Move Identifier')
 
     def is_valid_location(self, location):
         return self._game.state[location[1]][location[0]] in [0, 8]
