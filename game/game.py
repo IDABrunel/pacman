b1d33ac7aa60c6ba09563afbeb9442c2bd415998
BhaveetNagaria <12448410+BhaveetNagaria@users.noreply.github.com>
2019-12-06 00:30:45 +0000

    Fix for E501

--- game/game.py
+++ game/game.py
@@ -118,7 +118,10 @@ class Game:
     def calc_score(self):
         nuggets_score = ((176 - self.count_nuggets_left()) * 5)
         fruit_score = ((self.pacman._fruit_collected) * 100)
-        caught_ghosts = ((_num_time_blinky_caught * 40) + (_num_time_clyde_caught * 40) + (_num_time_inky_caught * 40) + (_num_time_pinky_caught * 40))
+        caught_ghosts = ((self._num_time_blinky_caught * 40) +
+                         (self._num_time_clyde_caught * 40) +
+                         (self._num_time_inky_caught * 40) +
+                         (self._num_time_pinky_caught * 40))
 
         return nuggets_score + fruit_score + caught_ghosts
 
