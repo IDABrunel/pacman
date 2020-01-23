d9bea45494c8de59123abb9b7347a5133e8822d5
Ben Evans <ben@bluechimp.io>
2020-01-23 11:55:56 +0000

    Command & Conquer (#55)
    * chore: remove nugget from start position
    
    * chore: moves to own files, only import on use
    
    * feat: support for usb gamepad controller
    
    * docs: remove old info from readme
    
    * feat: custom styles

--- game/run.py
+++ game/run.py
@@ -3,9 +3,8 @@ import argparse
 from matplotlib import pyplot as plt
 from game import Game
 from arduino import ArduinoRGBMatrix
-from moves import FullRandom, ValidRandom, ValidRandomWithMomentem, UserInput
 from results_display import generate_board_with_stats
-
+from moves.rand import ValidRandomWithMomentem
 
 INIT_BOARD_STATE = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
@@ -18,7 +17,7 @@ INIT_BOARD_STATE = [
     [1, 8, 0, 9, 1, 8, 0, 8, 0, 8, 0, 1, 1, 8, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 1, 1, 8, 1, 8, 0, 8, 1, 8, 0, 8, 0, 0, 0, 1, 0, 1, 1, 1, 1, 8, 1, 1, 0, 1, 0, 8, 0, 1],
     [1, 0, 1, 0, 1, 0, 1, 0, 1, 0, 8, 0, 8, 0, 8, 0, 8, 1, 0, 1, 8, 1, 8, 1, 8, 1, 0, 1, 8, 1, 8, 1, 1, 0, 1, 0, 8, 0, 1, 0, 8, 0, 1, 1, 8, 1, 8, 0, 8, 0, 8, 0, 1, 0, 8, 0, 9, 0, 8, 1],
     [1, 8, 1, 8, 1, 8, 1, 8, 1, 8, 1, 1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 0, 8, 1, 8, 1, 1, 1, 8, 1, 1, 1, 8, 1, 1, 0, 1, 0, 1, 1, 1, 1, 8, 1, 8, 1, 1, 1, 1, 0, 1],
-    [1, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 1],
+    [1, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 0, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 0, 8, 1],
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1],
 ]
 
@@ -40,6 +39,10 @@ board = Game(
 
 
 parser = argparse.ArgumentParser()
+parser.add_argument('--style',
+                    type=str,
+                    default='default',
+                    help='Style to use')
 parser.add_argument('--matplotlib', action='store_true',
                     help='Enables matplotlib output')
 parser.add_argument('--arduino', action='store_true',
@@ -54,7 +57,7 @@ parser.add_argument('--images', action='store_true',
                     help='Saves each figure to ./images/xxxx.png')
 parser.add_argument('--strategy',
                     type=str,
-                    choices=['full_random', 'valid_random', 'valid_random_momentem', 'user_input'],
+                    choices=['full_random', 'valid_random', 'valid_random_momentem', 'user_input', 'controller'],
                     default='valid_random_momentem',
                     help='Pacman move strategy')
 
@@ -70,13 +73,20 @@ inky_move_factory = ValidRandomWithMomentem()
 clyde_move_factory = ValidRandomWithMomentem()
 
 if args.strategy == 'full_random':
+    from moves.rand import FullRandom
     pacman_move_factory = FullRandom()
 elif args.strategy == 'valid_random':
+    from moves.rand import ValidRandom
     pacman_move_factory = ValidRandom()
 elif args.strategy == 'valid_random_momentem':
+    from moves.rand import ValidRandomWithMomentem
     pacman_move_factory = ValidRandomWithMomentem()
 elif args.strategy == 'user_input':
-    pacman_move_factory = UserInput()
+    from moves.console import ConsoleInput
+    pacman_move_factory = ConsoleInput()
+elif args.strategy == 'controller':
+    from moves.controller import Controler
+    pacman_move_factory = Controler()
 else:
     raise 'Unknown movmement strategy.'
 
@@ -85,21 +95,23 @@ if args.arduino:
     arduino_matrix = ArduinoRGBMatrix(args.arduino_path)
     arduino_matrix.clear()
     arduino_matrix.update_by_n_random_pixels(
-        generate_board_with_stats(board),
+        generate_board_with_stats(board, args.style),
         50
     )
 
 if args.matplotlib:
     plt.ion()
     plt.clf()
-    plt.imshow(generate_board_with_stats(board))
+    plt.imshow(generate_board_with_stats(board, args.style))
+    plt.axis('off')
     plt.show()
     plt.pause(0.05)
 
 if args.images:
     plt.clf()
-    plt.imshow(generate_board_with_stats(board))
-    plt.savefig('images/0000.png')
+    plt.imshow(generate_board_with_stats(board, args.style))
+    plt.axis('off')
+    plt.savefig('images/0000.png', bbox_inches='tight')
 
 while board.complete is False:
     print('Tick...' + str(board._current_tick))
@@ -112,15 +124,17 @@ while board.complete is False:
     )
 
     if args.arduino:
-        arduino_matrix.update(generate_board_with_stats(board))
+        arduino_matrix.update(generate_board_with_stats(board, args.style))
 
     if args.matplotlib:
         plt.clf()
-        plt.imshow(generate_board_with_stats(board))
+        plt.imshow(generate_board_with_stats(board, args.style))
+        plt.axis('off')
         plt.show()
         plt.pause(0.05)
 
     if args.images:
         plt.clf()
-        plt.imshow(generate_board_with_stats(board))
-        plt.savefig('images/{:04d}.png'.format(board._current_tick))
+        plt.imshow(generate_board_with_stats(board, args.style))
+        plt.axis('off')
+        plt.savefig('images/{:04d}.png'.format(board._current_tick), bbox_inches='tight')
---
+++ game/styles/default.py
@@ -0,0 +1,20 @@
+STYLE = {
+    0: [0, 0, 0],  # space
+    1: [0, 0, 50],  # wall
+    2: [255, 193, 7],  # pacman
+    3: [244, 67, 54],  # blinky
+    4: [233, 30, 30],  # pinky
+    5: [0, 188, 212],  # inky
+    6: [255, 87, 34],  # clyde
+    7: [128, 128, 128],  # ghostGate
+    8: [10, 10, 0],  # nuggets
+    9: [255, 255, 255],  # ghost killing nugget
+    10: [34, 139, 34],  # fruit
+    11: [0, 0, 128],  # Ghost Panic One
+    12: [0, 128, 128],  # Ghost Panic Two
+
+    'STATS_ENABLED': True,
+    'STATS_TEXT': (100, 100, 0),
+    'STATS_BG': (0, 0, 0),
+    'STATS_REDUCER': 5,
+}
